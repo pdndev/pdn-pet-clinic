cfb43010e8494131e1fccb89a76f04e8c1207129
Arunkumar Arjunan <arunkumar.a@prodapt.com>
2019-12-25 18:17:31 +0530

    added

---
+++ src/main/java/com/pro/app/PdnPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.pro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdnPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdnPetClinicApplication.class, args);
+	}
+
+}
